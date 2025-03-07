97d6070ae5ee77ff0f9a1d00d72c49bbf1493dc1
clovis <clovis_arroyo@hotmail.com>
2025-03-07 18:08:33 -0300

    feat(script): script for the main page written.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	let message = '';
+	let messages = writable([]);
+	let ws;
+
+	onMount(() => {
+		ws = new WebSocket('AQUI NOSSO BACKEND :)');
+
+		ws.onmessage = (event) => {
+			messages.update((msgs) => [...msgs, JSON.parse(event.data)]);
+		};
+	});
+
+	function sendMessage() {
+		if (message.trim() !== '') {
+			ws.send(JSON.stringify({ text: message, sender: 'User' }));
+			message = '';
+		}
+	}
+</script>
